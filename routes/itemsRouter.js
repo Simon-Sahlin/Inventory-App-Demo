a01c3eaef0ae3d867e42719161f252f4b52ebc87
Simon-Sahlin <simonsahlin@somin.se>
2024-08-13 17:22:08 +0200

    Add dummy new item form

--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -4,28 +4,32 @@ const router = Router();
 const { items } = require("../utils/dummyData");
 
 
-router.get("/newItem", (req, res) => {
-    res.send("WIP")
-});
+
+
+
+// router.get("/:itemId/editItem", (req, res) => {
+//     res.send("WIP")
+// });
+
+// router.post("/:itemId/updateItem", (req, res) => {
+//     res.send("WIP")
+// });
+
+// router.post("/:itemId/deleteItem", (req, res) => {
+//     res.send("WIP")
+// });
+
+// router.get("/:itemId", (req, res) => {
+//     res.send("WIP")
+// });
+
 
 router.post("/createItem", (req, res) => {
     res.send("WIP")
 });
 
-router.get("/:itemId/editItem", (req, res) => {
-    res.send("WIP")
-});
-
-router.post("/:itemId/updateItem", (req, res) => {
-    res.send("WIP")
-});
-
-router.post("/:itemId/deleteItem", (req, res) => {
-    res.send("WIP")
-});
-
-router.get("/:itemId", (req, res) => {
-    res.send("WIP")
+router.get("/new", (req, res) => {
+    res.render("items/newItem");
 });
 
 router.get("/", (req, res) => {
